e33991beebfafe7f54f7e2819fc92d490051fb39
charles-okunzo <okunzopeter01@gmail.com>
2023-10-17 11:32:38 +0300

    style: add login template

--- Warehouse_MS/urls.py
+++ Warehouse_MS/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from auth import views as auth_views
+from users import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
