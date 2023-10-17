d6cea608d54393be844cfad58fd369502b2c9e5b
charles-okunzo <okunzopeter01@gmail.com>
2023-10-17 09:35:34 +0300

    ft: add auth-login template

--- Warehouse_MS/urls.py
+++ Warehouse_MS/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from ..auth.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing_page.urls')),
+    path('login', include())
 ]
