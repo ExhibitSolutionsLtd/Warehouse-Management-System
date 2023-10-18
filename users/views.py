f876f0a426612380e7936ea7f304164d07ec3902
charles-okunzo <okunzopeter01@gmail.com>
2023-10-18 10:36:46 +0300

    ft:create functional register page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
 def login(request):
     return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created Successfully {username}!')
+            return redirect('dashboard')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'register.html', {'form':form})
