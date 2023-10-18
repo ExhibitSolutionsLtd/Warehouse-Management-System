72299e19dfd70b4ff6f192fb6d1975f37d32e4a0
charles-okunzo <okunzopeter01@gmail.com>
2023-10-18 11:28:55 +0300

    ft:add logout page

--- Warehouse_MS/urls.py
+++ Warehouse_MS/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('landing_page.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 
 ]
